9cdb38d6f8cf90fa8ddc1af954c66620aef37252
bitzok <santacruzedward@hotmail.com>
2026-08-03 12:16:26 -0500

    chore: initial spring boot project setup

---
+++ src/test/java/com/utraxxer/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utraxxer.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
